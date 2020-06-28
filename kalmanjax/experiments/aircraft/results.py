7cec9c13292f482560caf9289eab99a9a47c927a
William Wilkinson <william.wilkinson@aalto.fi>
2020-06-28 15:31:08 +0300

    format experimental output

--- kalmanjax/experiments/aircraft/results.py
+++ kalmanjax/experiments/aircraft/results.py
@@ -7,5 +7,7 @@ for method in range(17):
         with open("output/" + str(method) + "_" + str(fold) + "_nlpd.txt", "rb") as fp:
             method_nlpd[method, fold] = pickle.load(fp)
 
-print(np.mean(method_nlpd, axis=1))
-print(np.std(method_nlpd, axis=1))
+np.set_printoptions(precision=3)
+print(np.nanmean(method_nlpd, axis=1))
+np.set_printoptions(precision=2)
+print(np.nanstd(method_nlpd, axis=1))
--- kalmanjax/experiments/binary/results.py
+++ kalmanjax/experiments/binary/results.py
@@ -7,5 +7,7 @@ for method in range(17):
         with open("output/" + str(method) + "_" + str(fold) + "_nlpd.txt", "rb") as fp:
             method_nlpd[method, fold] = pickle.load(fp)
 
+np.set_printoptions(precision=3)
 print(np.mean(method_nlpd, axis=1))
+np.set_printoptions(precision=2)
 print(np.std(method_nlpd, axis=1))
